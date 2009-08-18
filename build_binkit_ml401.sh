e446d32a812e304160ec403cf7fab9ab0d006d14
lekernel <sebastien.bourdeauducq@lekernel.net>
2009-08-18 19:02:54 +0200

    Rename binkit

--- build_binkit_ml401.sh
+++ build_binkit_ml401.sh
@@ -73,8 +73,8 @@ echo "OK"
 
 # Make a tarball
 echo -n "Generating tarball..."
-rm -f binkit-ml401.tar.bz2
-tar cjf binkit-ml401.tar.bz2 binkit
+rm -f binkit_ml401.tar.bz2
+tar cjf binkit_ml401.tar.bz2 binkit
 echo "OK"
 
 echo ""
