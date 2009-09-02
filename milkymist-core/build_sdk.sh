3139de3a57ff0d86e8ad3d1d30fa0689d105b3b9
lekernel <sebastien.bourdeauducq@lekernel.net>
2009-09-02 18:58:02 +0200

    Build SDK script

---
+++ milkymist-core/build_sdk.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+BASEDIR=`pwd`
+LOGFILEHOST=$BASEDIR/tools.log
+LOGFILE=$BASEDIR/software.log
+
+echo "================================================================================"
+echo "Building Milkymist SDK"
+echo ""
+echo "Log file (host):   $LOGFILEHOST"
+echo "Log file (target): $LOGFILE"
+echo "================================================================================"
+echo ""
+
+BASEDIR=`pwd`
+
+echo -n "Building host utilities..."
+cd $BASEDIR/tools
+make > $LOGFILEHOST 2>&1
+if [ "$?" != 0 ] ; then
+        echo "FAILED"
+	exit 1
+else
+        echo "OK"
+fi
+
+echo -n "Building base library..."
+cd $BASEDIR/software/baselib && make > $LOGFILE 2>&1
+if [ "$?" != 0 ] ; then
+        echo "FAILED"
+	exit 1
+else
+        echo "OK"
+fi
+echo -n "Building math library..."
+cd $BASEDIR/software/mathlib && make >> $LOGFILE 2>&1
+if [ "$?" != 0 ] ; then
+        echo "FAILED"
+	exit 1
+else
+        echo "OK"
+fi
+
+cd $BASEDIR
+
+echo "Build complete!"
