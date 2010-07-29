f0f224f1d9dd8e8374241ec16953f68705d83eaa
lekernel <sebastien.bourdeauducq@lekernel.net>
2010-07-29 20:00:29 +0200

    Working CRC5 function

--- softusb-ohci/crc.c
+++ softusb-ohci/crc.c
@@ -1,27 +1,48 @@
-/* Based on http://www.lvr.com/files/usb_crc.c by Ron Hemphill */
+/*
+ * Milkymist VJ SoC (OHCI firmware)
+ * Copyright (C) 2007, 2008, 2009, 2010 Sebastien Bourdeauducq
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-#define INT_SIZE 32
-unsigned char usb_crc5(unsigned long int input, int bit_count)
+unsigned char usb_crc5(unsigned char b1, unsigned char b2)
 {
-	const unsigned long int poly5 = (0x05L << (INT_SIZE-5));
-	unsigned long int crc5  = (0x1fL << (INT_SIZE-5));
-	unsigned long int udata = (input << (INT_SIZE-bit_count));
+	unsigned char crc;
+	unsigned char nextb;
+	unsigned char so;
+	unsigned char i;
 
-	while(bit_count--) {
-		// bit4 != bit4?
-		if((udata ^ crc5) & (0x1L<<(INT_SIZE-1))) {
-			crc5 <<= 1;
-			crc5 ^= poly5;
-		} else
-			crc5 <<= 1;
-		udata <<= 1;
+	crc = 0x1f;
+
+	for(i=0;i<8;i++) {
+		nextb = b1 & 0x01;
+		b1 >>= 1;
+		so = crc & 0x01;
+		crc >>= 1;
+		if(nextb != so)
+			crc ^= 0x14;
+	}
+	for(i=0;i<3;i++) {
+		nextb = b2 & 0x01;
+		b2 >>= 1;
+		so = crc & 0x01;
+		crc >>= 1;
+		if(nextb != so)
+			crc ^= 0x14;
 	}
 
-	// Shift back into position
-	crc5 >>= (INT_SIZE-5);
+	crc &= 0x1f;
+	crc ^= 0x1f;
 
-	// Invert contents to generate crc field
-	crc5 ^= 0x1f;
-
-	return crc5;
+	return crc;
 }
--- softusb-ohci/crc.h
+++ softusb-ohci/crc.h
@@ -1,6 +1,23 @@
+/*
+ * Milkymist VJ SoC (OHCI firmware)
+ * Copyright (C) 2007, 2008, 2009, 2010 Sebastien Bourdeauducq
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef __CRC_H
 #define __CRC_H
 
-unsigned char usb_crc5(unsigned int input, int bit_count);
+unsigned char usb_crc5(unsigned char b1, unsigned char b2);
 
 #endif /* __CRC_H */
