01b2c9e3524f7964aa91c5d0732b36089b0387d8
Sebastien Bourdeauducq <sebastien@milkymist.org>
2011-03-08 16:36:33 +0100

    Add GPL headers

--- cores/lm32/rtl/jtag_cores.v
+++ cores/lm32/rtl/jtag_cores.v
@@ -1,3 +1,20 @@
+/*
+ * Milkymist SoC
+ * Copyright (C) 2010 Michael Walle
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 module jtag_cores (
     input [7:0] reg_d,
     input [2:0] reg_addr_d,
