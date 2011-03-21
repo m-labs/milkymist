079aeabfcb6e6eebea8e5113ddc8dec8009732a7
Sebastien Bourdeauducq <sebastien@milkymist.org>
2011-03-21 15:21:39 +0100

    inline irq helper functions (Michael Walle)

--- software/include/base/irq.h
+++ software/include/base/irq.h
@@ -18,13 +18,45 @@
 #ifndef __IRQ_H
 #define __IRQ_H
 
-void irq_enable(unsigned int en);
-void irq_setmask(unsigned int mask);
-unsigned int irq_getmask();
-unsigned int irq_pending();
-void irq_ack(unsigned int mask);
+static inline void irq_enable(unsigned int en)
+{
+       __asm__ __volatile__("wcsr IE, %0" : : "r" (en));
+}
 
-unsigned int irq_getie();
-void irq_setie(unsigned int ie);
+static inline unsigned int irq_getmask()
+{
+       unsigned int mask;
+       __asm__ __volatile__("rcsr %0, IM" : "=r" (mask));
+       return mask;
+}
+
+static inline void irq_setmask(unsigned int mask)
+{
+       __asm__ __volatile__("wcsr IM, %0" : : "r" (mask));
+}
+
+static inline unsigned int irq_pending()
+{
+       unsigned int pending;
+       __asm__ __volatile__("rcsr %0, IP" : "=r" (pending));
+       return pending;
+}
+
+static inline void irq_ack(unsigned int mask)
+{
+       __asm__ __volatile__("wcsr IP, %0" : : "r" (mask));
+}
+
+static inline unsigned int irq_getie()
+{
+       unsigned int ie;
+       __asm__ __volatile__("rcsr %0, IE" : "=r" (ie));
+       return ie;
+}
+
+static inline void irq_setie(unsigned int ie)
+{
+       __asm__ __volatile__("wcsr IE, %0" : : "r" (ie));
+}
 
 #endif /* __IRQ_H */
