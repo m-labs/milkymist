b452f54fa1a7b8ea836711df86b634854f7a94c6
Werner Almesberger <werner@almesberger.net>
2011-11-30 06:45:39 -0300

    softusb: protect macro arguments in comloc.h
    This change is purely cosmetic.

--- softusb-input/comloc.h
+++ softusb-input/comloc.h
@@ -22,10 +22,10 @@
 #define COMLOCV(x)	(*(volatile unsigned char *)(x))
 
 #define COMLOC_DEBUG_PRODUCE	COMLOC(0x1000)
-#define COMLOC_DEBUG(offset)	COMLOC(0x1001+offset)
+#define COMLOC_DEBUG(offset)	COMLOC(0x1001+(offset))
 #define COMLOC_MEVT_PRODUCE	COMLOC(0x1101)
-#define COMLOC_MEVT(offset)	COMLOC(0x1102+offset)
+#define COMLOC_MEVT(offset)	COMLOC(0x1102+(offset))
 #define COMLOC_KEVT_PRODUCE	COMLOC(0x1142)
-#define COMLOC_KEVT(offset)	COMLOC(0x1143+offset)
+#define COMLOC_KEVT(offset)	COMLOC(0x1143+(offset))
 
 #endif /* __COMLOC_H */
